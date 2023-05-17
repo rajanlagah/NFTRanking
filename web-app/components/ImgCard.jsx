49a4dbc3fbb4ea87a9f2cf9c47334c9cf37cd0bc
rajanlagah <rajanlagah@gmail.com>
2023-05-17 18:55:05 +0530

    added rank score map and buy option

--- web-app/components/ImgCard.jsx
+++ web-app/components/ImgCard.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./../styles/Home.module.css";
 
-const ImgCard = ({ metadata, total_rarity_score }) => {
+const ImgCard = ({
+  metadata,
+  total_rarity_score,
+  rarity_map,
+  token_address,
+  token_id
+}) => {
+  const [showRankMap, setshowRankMap] = useState(true);
   const getImgData = () => {
     const data = JSON.parse(metadata);
     let imgURI;
@@ -16,10 +23,19 @@ const ImgCard = ({ metadata, total_rarity_score }) => {
     return { imgURI, rarityScore: total_rarity_score, name: data.name };
   };
 
+  const handleOnByClick = () => {
+    const url = `https://opensea.io/assets/ethereum/${token_address}/${token_id}`;
+    window.open(url, "_blank");
+  };
+
   const { imgURI, rarityScore, name } = getImgData();
 
   return (
-    <div className={styles["img-card"]}>
+    <div
+      className={styles["img-card"]}
+      onMouseEnter={() => setshowRankMap(true)}
+      onMouseLeave={() => setshowRankMap(false)}
+    >
       <img
         // alt="Impostors Genesis Aliens"
         src={imgURI}
@@ -27,8 +43,28 @@ const ImgCard = ({ metadata, total_rarity_score }) => {
       />
       <div className={styles["imposters-card-info"]}>
         <h5>{name}</h5>
-        <h4 className={styles["imposters-card-score"]}>{rarityScore.toFixed(2)}</h4>
+        <h4 className={styles["imposters-card-score"]}>
+          {rarityScore.toFixed(2)}
+        </h4>
       </div>
+      {showRankMap && (
+        <div className={styles["img-card-score-details"]}>
+          <div>
+            {rarity_map.map((data) => (
+              <React.Fragment>
+                <div className={styles.flex}>
+                  <span>
+                    {data.variant} {data.trait}
+                  </span>
+                  <span>{data.score.toFixed(2)}</span>
+                  <br />
+                </div>
+              </React.Fragment>
+            ))}
+          </div>
+          <button onClick={handleOnByClick} className={styles["img-card-score-details-btn"]}> Buy</button>
+        </div>
+      )}
     </div>
   );
 };
